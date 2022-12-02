0f6dfb6cdc97daaa9ea7ed9d0f25b82f88629812
omerokumus <omerokumus3@gmail.com>
2022-12-02 09:14:59 +0300

    Initial Commit

---
+++ ImRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImRich
+//
+//  Created by Ömer Faruk Okumuş on 2.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
